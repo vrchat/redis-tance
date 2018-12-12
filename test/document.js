2493404d5db4994e0e33bec9a6f1b70d6ab33e8a
Curtis Lassam <curtis@lassam.net>
2018-12-12 11:14:40 -0800

    Test automatic schema upgrade in documents

--- test/document.js
+++ test/document.js
@@ -76,4 +76,55 @@ describe("Document tests", function() {
         }
     });
 
+    it("Set an employee, then change the schema", async function() {
+        let empl = {
+            "type": "object",
+            "properties": {
+                "id": {"type": "string"},
+                "type": {"type": "string"},
+                "version": {"type": "integer"},
+                "firstname": {"type": "string"},
+                "lastname": {"type": "string"},
+            },
+            "required": ["id", "type", "version", "firstname", "lastname"]
+        };
+
+        let employeeSchema = new Skeema({type: "Employee", v1: empl});
+
+        let document = tance.document({schema: employeeSchema});
+
+        let employee = {
+            "firstname": "Dang",
+            "lastname": "Son",
+        };
+
+        let setEmployee = await document.set(employee);
+        let id = setEmployee.id;
+
+        let empl2 = {
+            "type": "object",
+            "properties": {
+                "id": {"type": "string"},
+                "type": {"type": "string"},
+                "version": {"type": "integer"},
+                "firstname": {"type": "string"},
+                "lastname": {"type": "string"},
+                "fullname": {"type": "string"},
+            },
+            "required": ["id", "type", "version", "firstname", "lastname", "fullname"]
+        };
+
+        let newEmployeeSchema = new Skeema({type: "Employee", v1: empl});
+        newEmployeeSchema.addVersion(empl2, employee => {
+            employee.fullname = `${employee.firstname} ${employee.lastname}`;
+            return employee;
+        });
+
+        let newDocument = tance.document({schema: newEmployeeSchema, id: id});
+        let getEmployee = await newDocument.get();
+
+        // when we get the OLD document, it should, as if by magic, conform to the new schema
+        assert.equal(getEmployee.fullname, "Dang Son");
+    });
+
 });
