cd85b71e4893d8ba8ca732d77b6f7854fa0f08af
Curtis Lassam <curtis@lassam.net>
2018-12-14 13:19:41 -0800

    This is some magic right here

--- test/widget.js
+++ test/widget.js
@@ -36,6 +36,113 @@ class WidgetTable extends Table{
     };
 }
 
+class WidgetTableV3 extends Table{
+    constructor({tance, namespace=""}){
+        let widgetSchemaV1 = {
+            "type": "object",
+            "properties": {
+                // these three properties are mandatory for every object
+                "id": {"type": "string"},
+                "type": {"type": "string"},
+                "version": {"type": "integer"},
+                // these are our real properties:
+                "widgetOwnerId": {"type": "string", "index": "simple"},
+                "widgetName": {"type": "string", "index": "simple"},
+                // note that widgetOptional isn't in "required"
+                "widgetOptional": {"type": "string", "index": "simple"},
+                "created_at_timestamp": {"type": "integer", "minimum": 0},
+                "created_at_iso": {"type": "string"},
+            },
+            "additionalProperties": false,
+            "required": ["id", "type", "version", "widgetOwnerId", "widgetName", "created_at_timestamp", "created_at_iso"]
+        };
+        // added Polarity, Molarity, and Hilarity, Polarity is mandatory
+        let widgetSchemaV2 = {
+            "type": "object",
+            "properties": {
+                // these three properties are mandatory for every object
+                "id": {"type": "string"},
+                "type": {"type": "string"},
+                "version": {"type": "integer"},
+                // these are our real properties:
+                "widgetOwnerId": {"type": "string", "index": "simple"},
+                "widgetName": {"type": "string", "index": "simple"},
+                // note that widgetOptional isn't in "required"
+                "widgetOptional": {"type": "string", "index": "simple"},
+                "widgetIsNamedTed": {"type": "boolean"},
+                "widgetPolarity": {"type": "boolean"},
+                "widgetMolarity": {"type": "integer"},
+                "widgetHilarity": {"type": "integer"},
+                "created_at_timestamp": {"type": "integer", "minimum": 0},
+                "created_at_iso": {"type": "string"},
+            },
+            "additionalProperties": false,
+            "required": ["id",
+                "type",
+                "version",
+                "widgetOwnerId",
+                "widgetName",
+                "widgetIsNamedTed",
+                "widgetPolarity",
+                "created_at_timestamp",
+                "created_at_iso"]
+        };
+        let v1ToV2 = (object) =>{
+            object.widgetIsNamedTed = object.widgetName.toLowerCase() === "ted";
+            object.widgetPolarity = false;
+            object.widgetHilarity = object.widgetIsNamedTed ? 100 : 20;
+            object.widgetMolarity = 0;
+            return object;
+        };
+        // removed Hilarity, added a list of tags and a rando object, and add an index on widgetMolarity
+        let widgetSchemaV3 = {
+            "type": "object",
+            "properties": {
+                // these three properties are mandatory for every object
+                "id": {"type": "string"},
+                "type": {"type": "string"},
+                "version": {"type": "integer"},
+                // these are our real properties:
+                "widgetOwnerId": {"type": "string", "index": "simple"},
+                "widgetName": {"type": "string", "index": "simple"},
+                // note that widgetOptional isn't in "required"
+                "widgetOptional": {"type": "string", "index": "simple"},
+                "widgetIsNamedTed": {"type": "boolean"},
+                "widgetPolarity": {"type": "boolean"},
+                "widgetMolarity": {"type": "integer", index: 'simple'},
+                "tags": {"type": "array", "maxItems": 10, "items": {"type": "string"}},
+                "whatever": {"type": "object"},
+                "created_at_timestamp": {"type": "integer", "minimum": 0},
+                "created_at_iso": {"type": "string"},
+            },
+            "additionalProperties": false,
+            "required": ["id",
+                "type",
+                "version",
+                "widgetOwnerId",
+                "widgetName",
+                "widgetIsNamedTed",
+                "widgetPolarity",
+                "tags",
+                "whatever",
+                "created_at_timestamp",
+                "created_at_iso"]
+        };
+        let v2ToV3 = (object) =>{
+            object.tags = [];
+            object.whatever = {};
+            delete object.widgetHilarity;
+            return object;
+        };
+
+        let skeema = new Skeema({type: "Widget", v1: widgetSchemaV1});
+        skeema.addVersion(widgetSchemaV2, v1ToV2);
+        skeema.addVersion(widgetSchemaV3, v2ToV3);
+
+        super({tance, skeema, namespace});
+    };
+}
+
 let tance = null;
 
 describe("Widget tests", function() {
@@ -235,9 +342,92 @@ describe("Widget tests", function() {
         assert.equal(results.length, 2);
     });
 
-    // TODO: test modify
+    it("Create, modify, retrieve, and find a widget, in a namespace", async function () {
+        let table = new WidgetTable({tance, namespace: "gorg"});
+        let widgeotto = await table.insert({
+            "widgetOwnerId": "user-"+uuid(),
+            "widgetName": "testWidget",
+            "created_at_timestamp": luxon.local().valueOf(),
+            "created_at_iso": luxon.local().toString(),
+        });
+
+        let otherThing = await table.insert({
+            "widgetOwnerId": "user-"+uuid(),
+            "widgetName": "brian",
+            "created_at_timestamp": luxon.local().valueOf(),
+            "created_at_iso": luxon.local().toString(),
+        });
+
+        assert.exists(widgeotto.id);
+        assert.equal(widgeotto.type, "Widget");
+        assert.equal(widgeotto.version, 1);
+        assert.exists(widgeotto.widgetOwnerId);
+        assert.exists(widgeotto.created_at_timestamp);
+        assert.exists(widgeotto.created_at_iso);
+
+        let widgey = await table.modify(widgeotto.id, (object)=>{
+            object.widgetName = "bestWidget";
+            return object;
+        });
+
+        assert.exists(widgey.id);
+        assert.equal(widgey.type, "Widget");
+        assert.equal(widgey.version, 1);
+        assert.equal(widgey.widgetName, "bestWidget");
+        assert.exists(widgey.widgetOwnerId);
+        assert.exists(widgey.created_at_timestamp);
+        assert.exists(widgey.created_at_iso);
+
+        let widgeot = await table.get(widgeotto.id);
+
+        assert.exists(widgeot.id);
+        assert.equal(widgeot.type, "Widget");
+        assert.equal(widgeot.version, 1);
+        assert.equal(widgeot.widgetName, "bestWidget");
+        assert.exists(widgeot.widgetOwnerId);
+        assert.exists(widgeot.created_at_timestamp);
+        assert.exists(widgeot.created_at_iso);
+
+        let findWidge = await table.find({widgetName: "bestWidget"});
+
+        assert.equal(findWidge.length, 1);
+        assert.equal(findWidge[0].id, widgeotto.id);
+        assert.equal(findWidge[0].widgetName, "bestWidget");
+    });
+
+
+    it("Change the schema for a table and it should still work, I hope", async function () {
+        let table = new WidgetTable({tance});
+        let widgeotto = await table.insert({
+            "widgetOwnerId": "user-"+uuid(),
+            "widgetName": "Ted",
+            "created_at_timestamp": luxon.local().valueOf(),
+            "created_at_iso": luxon.local().toString(),
+        });
+
+
+        let upgradedTable = new WidgetTableV3({tance});
+
+        let widgey = await upgradedTable.get(widgeotto.id);
+        //console.warn("Retrieved:");
+        //console.warn(widgey);
+
+        assert.exists(widgey.id);
+        assert.equal(widgey.type, "Widget");
+        assert.equal(widgey.version, 3);
+        assert.exists(widgey.widgetOwnerId);
+        assert.exists(widgey.created_at_timestamp);
+        assert.exists(widgey.created_at_iso);
+
+        assert.equal(widgey.widgetIsNamedTed, true);
+        assert.exists(widgey.widgetPolarity);
+        assert.deepEqual(widgey.tags, []);
+        assert.deepEqual(widgey.whatever, {});
+    });
+
     // TODO: test document validation upgrade
-    // TODO: test upgrade indexes
+    // TODO: upgrade indexes on document upgrade
+    // TODO: recalculate indexes
 
     // TODO: a field that just holds an object or whatever
     // TODO: integer indexes?
