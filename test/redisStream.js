d50d360f4f849ad4176a60c5c49bb05a55b991dc
Curtis <curtis@lassam.net>
2019-06-25 00:29:55 -0700

    Read a limited number of things from the stream

--- test/redisStream.js
+++ test/redisStream.js
@@ -36,4 +36,47 @@ describe("Redis Stream Tests", function() {
         });
     });
 
+    it("Read a limited number of things from the stream", async function() {
+        let streamThurmond = new RedisStream({tance, channel:"zero", streamLength: 100, maximumNumberOfItemsToRead: 1});
+
+        let read = await streamThurmond.readFn();
+
+        await streamThurmond.write({
+            "gurb":"murb",
+        });
+        await streamThurmond.write({
+            "good":"jerb",
+        });
+        await streamThurmond.write({
+            "sassy":"lassy",
+        });
+
+        let results = await read();
+
+        assert.equal(results.length, 1);
+        assert.deepEqual(results[0], {
+            "gurb":"murb"
+        });
+    });
+
+    it("Read 3 things from the stream", async function() {
+        let streamThurmond = new RedisStream({tance, channel:"zero", streamLength: 100, maximumNumberOfItemsToRead: 3});
+
+        let read = await streamThurmond.readFn();
+
+        await streamThurmond.write({
+            "gurb":"murb",
+        });
+        await streamThurmond.write({
+            "good":"jerb",
+        });
+        await streamThurmond.write({
+            "sassy":"lassy",
+        });
+
+        let results = await read();
+
+        assert.equal(results.length, 3);
+    });
+
 });
